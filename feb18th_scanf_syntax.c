cf1c524170f5c97a799810ca12f88e7c68b8f699
samanthaotazo <164980722+samanthaotazo@users.noreply.github.com>
2025-02-19 09:48:58 -0500

    Create feb18th_scanf_syntax.c

---
+++ feb18th_scanf_syntax.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    char input1 = getchar();
+    
+    char input2;
+    int integerinput;
+    
+    scanf("%c", &input2);
+    scanf("%d", &integerinput)
+    return 0;
+}
