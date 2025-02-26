b6e68ece4a6e5afb1c1d0f83e35e41973c760846
samanthaotazo <164980722+samanthaotazo@users.noreply.github.com>
2025-02-26 09:44:12 -0500

    Update scanf_practice.c

--- scanf_practice.c
+++ scanf_practice.c
@@ -1,25 +1,26 @@
 /*What happens if the number of variables passed to the scanf function,
  * and/or the variable types, do not match those specified in the format
  * string? Write a simple program to verify your answer.
-Created by Samantha Otazo on September 25th.*/
+Created by Samantha Otazo on September 25th.
+* Modified 26 Feb 2025. */
 
 #include <stdio.h>
 
 int main() {
-    int int_input;
+    int int_input, int_input2;
     float float_input;
     char char_input;
 
     //prompt the user input:
     //printf("Type an integer here: ");
 
-    //scanf("%d", &int_input);
-    //printf("\nYou typed %d", int_input);
-    /*printf("Type a floating point value here: ");
+    /*scanf("%d", &int_input);
+    printf("\nYou typed %d", int_input);
+    printf("\nType a floating point value here: ");
     scanf("%d", &int_input); //here, we see format specifier type matches our variable
-
     printf("You typed %d", int_input); //we scanned in the truncated float value
     */
+
     //Advantages of scanf over getchar
     //multiple inputs
     char in1;
@@ -31,10 +32,23 @@ int main() {
 
     printf("You typed %c %c %c", in1, in2, in3);*/
     //inputs of certain type
-    int input = 0;
+    /*int input = 0;
 
     in1 = getchar(); //note that this would take in characters ONLY
-    scanf("%d", &input); //you can specify the format type
+    scanf("%d", &input); //you can specify the format type*/
 
+    /*printf("\nType in two numbers: ");
+    scanf("%d %d", &int_input);
+    printf("\nYou typed %d", int_input);*/
+
+    /*scanf("%d", &int_input, &int_input2);
+    printf("\nYou typed %d", int_input);*/
+
+    printf("\nType in one number: ");
+    //scanf("%f", &int_input);
+    //printf("Your number was %f", (float)int_input);
+
+    scanf("%d", &float_input);
+    printf("Your number was %f", float_input);
     return 0;
 }
